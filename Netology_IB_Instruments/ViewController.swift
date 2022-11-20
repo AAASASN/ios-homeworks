d5df0cf70625a8741ad2b5a6cdfd15ce65708cac
AAASASN <maraenko@gmail.com>
2022-11-20 13:34:01 +0300

    Initial Commit

---
+++ Netology_IB_Instruments/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Александр Мараенко on 20.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
